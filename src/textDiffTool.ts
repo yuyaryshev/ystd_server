57b5b4d3e49aa379f01bb8d80ad41b7214559437
Yuri Yaryshev <yuyaryshev@gmail.com>
2021-03-02 20:01:09 +0300

    push_all

---
+++ src/textDiffTool.ts
@@ -0,0 +1,17 @@
+import execa from "execa";
+
+export const text_diff_editor_npm_param_name = "text_diff_editor";
+
+export async function getTextDiffEditorPath(): Promise<string | undefined> {
+    const { stdout } = await execa("npm config get", [text_diff_editor_npm_param_name]);
+    return stdout !== "undefined" ? stdout : undefined;
+}
+
+export async function openFileDiffFromTextDiffEditor(baseFile: string, currentFilePath: string): Promise<boolean> {
+    const editorPath = await getTextDiffEditorPath();
+    if (editorPath) {
+        await execa(editorPath, [baseFile, currentFilePath]);
+        return true;
+    }
+    throw new Error(`CODE00000000 Path to ${text_diff_editor_npm_param_name} is not specified. Use 'npm config set ${text_diff_editor_npm_param_name} PATH_TO_EDITOR' to specify it.`)
+}
