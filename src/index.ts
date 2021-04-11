2fb21df3814c2b670f1359646bef28cbaa1921c6
Yuri Yaryshev <yuyaryshev@gmail.com>
2021-04-11 19:08:25 +0300

    push_all

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 export * from "ystd";
+
+// @INPRINT_START {exclude:"babelPresetsToPlugins"}
+export * from "./babelPresetsToPlugins";
 export * from "./deleteFileIfExists";
-export * from "./textDiffTool";
 export * from "./deleteFolderRecursive";
 export * from "./makeFunctionCov";
 export * from "./newSecureId";
 export * from "./projectDir";
 export * from "./readDirRecursive";
-export * from "./sha256";
-export * from "./yexec";
-export * from "./writeFileSyncIfChanged";
-export * from "./writeFileSerieSync";
-//export * from "./babelPresetsToPlugins";
-export * from "./writeFileSerieSync";
 export * from "./refreshFileSync";
+export * from "./sha256";
+export * from "./textDiffTool";
+export * from "./writeFileSerieSync";
+export * from "./writeFileSyncIfChanged";
+export * from "./yexec";
+// @INPRINT_END
