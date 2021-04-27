c1330933b5e442ab115763e9bf37aa472df50de7
Yuri Yaryshev <yuyaryshev@gmail.com>
2021-04-27 19:28:26 +0300

    2

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,18 @@
-import typescript from "rollup-plugin-typescript2";
+import typescript from "@rollup/plugin-typescript";
+import commonjs from "@rollup/plugin-commonjs";
+import nodeResolve from "@rollup/plugin-node-resolve";
 import pkg from "./package.json";
 
 export default [
-  {
-    input: "src/index.ts",
-    //extensions : [".js", ".jsx", ".ts", ".tsx"],
-    output: {
-      file: pkg.main,
-      format: "cjs",
-      sourcemap: true,
+    {
+        input: "src/index.ts",
+        //extensions : [".js", ".jsx", ".ts", ".tsx"],
+        output: {
+            file: pkg.main,
+            format: "cjs",
+            sourcemap: true,
+        },
+        plugins: [typescript({ module: "ESNext" }), commonjs(), nodeResolve()],
+        external: [...Object.keys(pkg.dependencies || {}), ...Object.keys(pkg.peerDependencies || {})],
     },
-    plugins: [
-      typescript({
-        typescript: require("typescript"),
-      }),
-    ],
-    external: [
-      ...Object.keys(pkg.dependencies || {}),
-      ...Object.keys(pkg.peerDependencies || {}),
-    ],
-  },
 ];
