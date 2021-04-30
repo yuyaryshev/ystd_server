e9f562a0ea0ac1abf7a2d3b469ac2325f4c68826
Yuri Yaryshev <yuyaryshev@gmail.com>
2021-04-30 20:15:57 +0300

    Added envFuncs.ts

---
+++ src/envFuncs.ts
@@ -0,0 +1,30 @@
+import deepMerge from "deepmerge";
+import {ManageableTimer} from "ystd";
+
+export type OnTerminateCallback = () => Promise<void> | void;
+
+export interface EnvBase {
+    onTerminateCallbacks: OnTerminateCallback[],
+    terminating: boolean;
+    timers: Set<ManageableTimer>;
+    terminate: () => Promise<void>;
+}
+
+export function emptyEnv(): EnvBase {
+    const pthis: EnvBase = {
+        onTerminateCallbacks: [],
+        terminating: false,
+        timers: new Set<ManageableTimer>(),
+        terminate: async () => {
+            pthis.terminating = true;
+            for (const timer of pthis.timers) timer.cancel();
+            for (const callback of pthis.onTerminateCallbacks) await callback();
+        },
+    };
+    return pthis;
+}
+
+export function mergeEnv<T1, T2>(existingEnv: T1 | undefined, addedEnv: T2): T1 & T2 {
+    const pthis = Object.assign(existingEnv || {}, deepMerge(addedEnv, existingEnv || {}));
+    return (pthis as any) as T1 & T2;
+}
