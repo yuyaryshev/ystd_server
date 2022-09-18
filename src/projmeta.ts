882f7f3bb0d0ea43a050d892505b93258a11bcd7
Yuri Yaryshev <yuyaryshev@gmail.com>
2022-09-18 20:36:04 +0300

    - Fixed writeFileSerieSync.ts

--- src/projmeta.ts
+++ src/projmeta.ts
@@ -1,2 +1,2 @@
-export const version = '1.5.1';
+export const version = '1.5.2';
 export const packageName = 'ystd_server';
--- src/writeFileSerieSync.ts
+++ src/writeFileSerieSync.ts
@@ -1,11 +1,18 @@
 import { outputFileSync, readdirSync, unlinkSync } from "fs-extra";
 import { join } from "path";
 
+export function dateToSerieSuffix(date0?: Date | string) {
+    let dateStr: string = typeof date0 === "string" ? date0 : (!date0 ? new Date() : date0).toISOString();
+    return dateStr.substring(0, 23).split(":").join("-").split("T").join("_").split(".").join("_");
+}
+
 export const writeFileSerieSync = (seriePath: string, fileName: string, content: string, maxFiles: number | undefined) => {
     if (maxFiles !== undefined && maxFiles > 0) {
         const files = readdirSync(seriePath).map((f: any): string => (typeof f === "string" ? f : f.name));
-        const delCount = maxFiles - 1 - files.length;
-        for (let i = 0; i < delCount; i++) unlinkSync(join(seriePath, files[i]));
+        const delCount = files.length - (maxFiles - 1);
+        for (let i = 0; i < delCount; i++) {
+            unlinkSync(join(seriePath, files[i]));
+        }
     }
     outputFileSync(join(seriePath, fileName), content, "utf-8");
 };
